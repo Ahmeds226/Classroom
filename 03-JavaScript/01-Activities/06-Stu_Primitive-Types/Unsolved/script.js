48666def8c1254cebde5a09873549a7c26c7f649
Ahmed Shahnawaz <iahmed.s1@icloud.com>
2022-04-14 18:46:29 +0100

    Week 1

--- 03-JavaScript/01-Activities/06-Stu_Primitive-Types/Unsolved/script.js
+++ 03-JavaScript/01-Activities/06-Stu_Primitive-Types/Unsolved/script.js
@@ -7,11 +7,17 @@ var six;
 
 // Insert comments to explain what each console log below will log to the console
 console.log(typeof one); 
+// Number
 console.log(typeof two); 
+// String
 console.log(typeof three); 
+// Boolean
 console.log(typeof four); 
+// Number
 console.log(typeof five);
+// String
 console.log(typeof six);
+// Undefined
 
 four = "Hello!";
 five = false;
@@ -19,5 +25,8 @@ six = 23;
 
 // Insert comments to explain what each console log below will log to the console
 console.log(typeof four);
+// String
 console.log(typeof five);
+// Boolean
 console.log(typeof six);
+// Number
