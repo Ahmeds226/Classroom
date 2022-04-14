7eac4c0daa590df0e5f6c7c1ad3f4be7cec8f529
Ahmed Shahnawaz <iahmed.s1@icloud.com>
2022-04-14 19:18:41 +0100

    Arrays

--- 03-JavaScript/01-Activities/Day 2/12-Stu_Arrays/Unsolved/script.js
+++ 03-JavaScript/01-Activities/Day 2/12-Stu_Arrays/Unsolved/script.js
@@ -1,1 +1,19 @@
 // WRITE YOUR CODE HERE
+var students = ["Bob", "Steve", "John", "Smith"];
+
+// 1
+console.log(students.length);
+
+// 2
+console.log("Welcome to the class", students[0])
+console.log("Welcome to the class", students[1])
+console.log("Welcome to the class", students[2])
+console.log("Welcome to the class", students[3])
+
+// 3
+students[0] = "Alex";
+console.log(students[0], "has replaced Bob");
+
+if (students[0] === "Alex") {
+    console.log(students[0], "is in class");
+}
