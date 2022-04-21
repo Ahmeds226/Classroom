2840e635bc771c621db620c2877d7e1c54edb314
Ahmed Shahnawaz <iahmed.s1@icloud.com>
2022-04-21 18:40:40 +0100

    Week 4

--- 03-JavaScript/01-Activities/Day 2/22-Stu_Objects/Unsolved/script.js
+++ 03-JavaScript/01-Activities/Day 2/22-Stu_Objects/Unsolved/script.js
@@ -1,1 +1,15 @@
 //WRITE YOUR CODE BELOW
+
+var DrinksOrder = {
+  DrinkName: "Mocha",
+  Sugars: 1,
+  Ready: true,
+};
+
+console.log(DrinksOrder.DrinkName, DrinksOrder.Sugars);
+
+if (DrinksOrder.Ready) {
+  console.log("Ready");
+} else {
+  console.log("Still in the queue");
+}
