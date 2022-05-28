1dc8e9b7634f5bfcb4d1e9d856633cf7eb1cd173
Ahmed Shahnawaz <iahmed.s1@icloud.com>
2022-05-28 10:31:22 +0100

    pull

---
+++ 05-Third-Party-APIs/04-Notes/session01/01-drill.js
@@ -0,0 +1,9 @@
+function countsVowels(str){
+    var count =0;
+    var vowels = [a, A, e, E, i, I, o, O, u, U];
+
+    for(var i =0, i < str.length; i++){
+        if(str.includes(vowels[i]))
+    }
+
+}
