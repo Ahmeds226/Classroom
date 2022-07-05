520104b9f651be8f3e662d143c11b3aa0a297b40
Ahmed Shahnawaz <iahmed.s1@icloud.com>
2022-07-05 18:59:15 +0100

    Week 14

--- 13-ORM/01-Activities/26-Stu_Literals/Unsolved/routes/api/readerRoutes.js
+++ 13-ORM/01-Activities/26-Stu_Literals/Unsolved/routes/api/readerRoutes.js
@@ -1,9 +1,9 @@
-const router = require('express').Router();
-const sequelize = require('../../config/connection');
-const { Reader, Book, LibraryCard } = require('../../models');
+const router = require("express").Router();
+const sequelize = require("../../config/connection");
+const { Reader, Book, LibraryCard } = require("../../models");
 
 // GET all readers
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const readerData = await Reader.findAll({
       include: [{ model: LibraryCard }, { model: Book }],
@@ -16,15 +16,25 @@ router.get('/', async (req, res) => {
 });
 
 // GET a single reader
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   try {
     const readerData = await Reader.findByPk(req.params.id, {
       include: [{ model: LibraryCard }, { model: Book }],
       // TODO: Add a sequelize literal to get a count of short books
+      attributes: {
+        include: [
+          [
+            sequelize.literal(
+              "(SELECT COUNT(*) FROM car WHERE book.reader_id = reader.id)"
+            ),
+            "totalMileage",
+          ],
+        ],
+      },
     });
 
     if (!readerData) {
-      res.status(404).json({ message: 'No reader found with that id!' });
+      res.status(404).json({ message: "No reader found with that id!" });
       return;
     }
 
@@ -35,7 +45,7 @@ router.get('/:id', async (req, res) => {
 });
 
 // CREATE a reader
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   try {
     const readerData = await Reader.create(req.body);
     res.status(200).json(readerData);
@@ -45,7 +55,7 @@ router.post('/', async (req, res) => {
 });
 
 // DELETE a reader
-router.delete('/:id', async (req, res) => {
+router.delete("/:id", async (req, res) => {
   try {
     const readerData = await Reader.destroy({
       where: {
@@ -54,7 +64,7 @@ router.delete('/:id', async (req, res) => {
     });
 
     if (!readerData) {
-      res.status(404).json({ message: 'No reader found with that id!' });
+      res.status(404).json({ message: "No reader found with that id!" });
       return;
     }
 
